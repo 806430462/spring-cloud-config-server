eca8046e1e933c79bc87f7e76fb023bf574054ef
806430462 <806430462@qq.com>
2019-01-02 19:20:23 +0800

    change something

--- src/main/java/com/yonyougov/configserver/ConfigServerApp.java
+++ src/main/java/com/yonyougov/configserver/ConfigServerApp.java
@@ -4,9 +4,13 @@ package com.yonyougov.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableFeignClients
+@EnableEurekaClient
 public class ConfigServerApp {
 	
     public static void main(String[] args) {
